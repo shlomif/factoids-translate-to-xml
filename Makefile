4603373c5167f9df01c60facb13ad0a45cc1fd3d
Shlomi Fish <shlomif@shlomifish.org>
2014-09-23 21:08:33 +0300

    Add more.

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-all: f2.xml
+all: f2.xml f3.xml
 
 f2.xml: trans1.xslt Factoids.xml
 	xsltproc -o $@ trans1.xslt Factoids.xml
+
+f3.xml: Transform.pl f2.xml
+	perl Transform.pl f2.xml $@
---
+++ Transform.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use XML::LibXML;
+
+my ($in_fn, $out_fn) = @ARGV;
+
+my $p = XML::LibXML->new;
+
+my $doc = $p->parse_file($in_fn);
+
+my @nodes = $doc->findnodes('//he');
+
+foreach my $n (@nodes)
+{
+    my @childs = $n->childNodes();
+    my $elem = XML::LibXML::Element->new('f');
+    my $l_en = XML::LibXML::Element->new('l');
+    $l_en->setAttribute("xml:lang", "en-US");
+    my $l_he = XML::LibXML::Element->new('l');
+    $l_he->setAttribute("xml:lang", "he-IL");
+
+    my @n_prevs;
+
+    my $x = $n->previousSibling;
+    while ($x->localname ne 'en')
+    {
+        push @n_prevs, $x;
+    }
+    continue
+    {
+        $x = $x->previousSibling;
+    }
+    push @n_prevs, $x;
+
+    for my $c (@childs)
+    {
+        $l_he->appendChild($c);
+    }
+    for my $c ($n_prevs[-1]->childNodes)
+    {
+        $l_en->appendChild($c);
+    }
+
+    $elem->appendChild($l_en);
+    $elem->appendChild($l_he);
+
+    my $parent = $n->parentNode;
+    foreach my $c (@n_prevs)
+    {
+        $parent->removeChild( $c );
+    }
+    $parent->replaceChild(
+        $elem,
+        $l_he,
+    );
+
+}
+
+use IO::All qw/ io /;
+io->file($out_fn)->utf8->print($doc->toString);
